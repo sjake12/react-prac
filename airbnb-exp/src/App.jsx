98112689f82447dd8b517191ce3bc1c243d3e4c8
sjake12 <apostol.sjake12@gmail.com>
2024-08-30 13:58:21 +0800

    update

--- airbnb-exp/src/App.jsx
+++ airbnb-exp/src/App.jsx
@@ -1,7 +1,6 @@
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
 import Card from "./components/Card.jsx";
-import Dropdown from "./components/Dropdown.jsx";
 import data from "./data.js";
 import "./App.css";
 
@@ -15,7 +14,6 @@ function App() {
       <Navbar />
       <Hero />
       <section className="cards-list">{cards}</section>
-      {/* <Dropdown /> */}
     </>
   );
 }
