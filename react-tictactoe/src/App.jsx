87eb88fd473c62bcdbd8f4e34407e288270c67bf
sjake12 <apostol.sjake12@gmail.com>
2024-08-31 16:01:54 +0800

    initial commit

---
+++ react-tictactoe/src/App.jsx
@@ -0,0 +1,8 @@
+import Game from "./components/Game";
+import "./App.css";
+
+function App() {
+  return <Game />;
+}
+
+export default App;
---
+++ react-tictactoe/src/components/Square.jsx
@@ -0,0 +1,11 @@
+import "../App.css";
+
+function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+export default Square;
